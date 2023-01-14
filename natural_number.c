db3fee92496b648de85f0c386d1b99e3992a0d79
Monu <kumarmonu1812@gmail.com>
2023-01-14 22:51:31 +0530

    https://github.com/its-monuyadav/C-tutorial.git

---
+++ natural_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main () {
+    int n;
+    printf("enter number :");
+    scanf("%d" , &n);
+
+    if(n <= 0 ){
+        printf("not a natural number \n");
+    }
+    else{
+        printf("natural number \n");
+    }
+
+}
